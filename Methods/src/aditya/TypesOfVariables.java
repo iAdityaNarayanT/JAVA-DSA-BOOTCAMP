14d38f916c103f46d4ee5f72ccbef3469d30c0a8
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2022-06-04 21:54:42 +0530

    Modified TypesOfVariables.java

--- Methods/src/aditya/TypesOfVariables.java
+++ Methods/src/aditya/TypesOfVariables.java
@@ -1,21 +1,42 @@
 package aditya;
 
 public class TypesOfVariables {
-    static int a=10; // static variable
-    static int b=20; // static variable
-    int c=30; // instance variable
+    static int a=10; // static variable M1
+    static int b=20; // static variable M2
+
+    static int c=20; // static variable M3
+    int d =30; // instance variable
     public static void main(String[] args) {
-        int d = 40;
+        int e = 40;
+
         /*
         Static variable can be accessed in two ways inside a method:
-        1.directly accessed
-        2. accessed via class name
+        M1. directly accessed
+        M2. accessed via class name
+        M3. creation of object, then accessing the variable using that object
          */
-        System.out.println(a); // METHOD 1 of accessing static variables
-        System.out.println(TypesOfVariables.b); //METHOD 2 of accessing static variable using class name
-        TypesOfVariables object = new TypesOfVariables(); // making object of class which has the instance variable in order to access the instance variable
-        System.out.println(object.c); // accessed instance variable using object and dot operator
-        System.out.println(d); // local variable directly accessed
 
+        System.out.println(a); // M1 Directly accessing static variables
+        System.out.println(TypesOfVariables.b); // M2 of accessing static variable using class name
+
+        //!If we access the static variable like the Instance variable (through an object),
+        // the compiler will show the warning message, which won’t halt the program.
+        // The compiler will replace the object name with the class name automatically.
+        TypesOfVariables obj2 =new TypesOfVariables(); // M3 accessing static variable by creation of object obj2 of class TypesOfVariable
+        System.out.println(obj2.b);//
+
+        //now accessing instance variable
+        TypesOfVariables obj1 = new TypesOfVariables(); // making object of class which has the instance variable in order to access the instance variable
+        System.out.println(obj1.d); // accessed instance variable using object and dot operator
+
+        System.out.println(e); // local variable directly accessed
+
+
+        Demo obj2 = new Demo();
+        System.out.println(obj2.f); //accessing instance variable from another class
+
+    }
+    public static class Demo{
+        int f=50;
     }
 }
