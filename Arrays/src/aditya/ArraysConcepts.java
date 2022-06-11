fad371bc3a424b19828d18adabbdfff340321563
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2022-06-11 23:58:52 +0530

    Adding Arrays

--- Arrays/src/aditya/ArraysConcepts.java
+++
@@ -1,7 +0,0 @@
-package aditya;
-
-public class ArraysConcepts {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ Arrays/src/com/aditya/Main.java
@@ -0,0 +1,9 @@
+package com.aditya;
+
+public class Main {
+
+        public static void main(String[] args) {
+            String[] rnos = new String[5];
+            System.out.println(rnos[3]);
+        }
+}
