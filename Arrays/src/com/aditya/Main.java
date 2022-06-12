a011d89b0b93ae1c7bbaae188d18a9e93d9c4d73
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2022-06-12 18:23:20 +0530

    Adding Array file 1--> Main.java

--- Arrays/src/com/aditya/Main.java
+++ Arrays/src/com/aditya/Main.java
@@ -3,7 +3,15 @@ package com.aditya;
 public class Main {
 
         public static void main(String[] args) {
+//            Array Of an OBJECT type--> string
             String[] rnos = new String[5];
-            System.out.println(rnos[3]);
+            System.out.println(rnos[3]); // null--> by default value of empty array
+//          Array of Primitive type--> int
+            int[] rnos3 = new int[10];
+            System.out.println(rnos3[6]); // 0
+
+            int[] rnos2 = {1,2,3,4,5};
+            System.out.println(rnos2[2]); // 3
+
         }
 }
