a4ef9ec25a0346cdc1af008cfdf5048bf73d0ba3
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2022-07-03 17:57:34 +0530

    Adding pass by value concept wrt array

--- Arrays/src/com/aditya/PassingInFunctions.java
+++ Arrays/src/com/aditya/PassingInFunctions.java
@@ -4,15 +4,15 @@ public class PassingInFunctions {
     public static void main(String[] args) {
 
         int [] nums={1,2,3,4,5};
-        System.out.println(nums[2]);
+        System.out.println(nums[2]); // 3
         change(nums);
-        System.out.println(nums[2]);
+        System.out.println(nums[2]);//4
 
 
         String [] str ={"AA" , "BB" , "CC" , "DD" , "EE"};
-        System.out.println(str[4]);
+        System.out.println(str[4]);//EE
         changeString(str);
-        System.out.println(str[4]);
+        System.out.println(str[4]);//Logan
     }
 
     static void change(int[] numbers) {
