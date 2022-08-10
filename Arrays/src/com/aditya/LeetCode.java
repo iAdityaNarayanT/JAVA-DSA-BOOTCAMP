c719d82789b1f91fc8420b09693470951b49c79f
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2022-08-10 20:34:48 +0530

    Adding pass by value concept wrt array

--- Arrays/src/com/aditya/LeetCode.java
+++ Arrays/src/com/aditya/LeetCode.java
@@ -4,20 +4,15 @@ import java.util.Arrays;
 
 public class LeetCode {
     public static void main(String[] args) {
-        int[] nums ={1,2,1};
-        System.out.println(Arrays.toString(getConcatenation(nums)));
+        int[] nums ={8,1,2,2,3};
+//        System.out.println(Arrays.toString(peakElement(nums)));
+    }
+//    // Function to find the peak element
+//        // arr[]: input array
+//        // n: size of array a[]
+//        public static int[] peakElement(int[] nums){
+//
+//
+//        }
     }
 
-    static int[] getConcatenation(int[] nums) {
-        int n= nums.length;
-        int[] ans= new int[2*n];
-        for (int i = 0; i <n; i++) {
-            ans[i]=nums[i];
-            ans[i+n]=nums[i];
-        }
-//        for (int i = 0; i <n; i++) {
-//            ans[i+n]=nums[i];
-//        }
-        return ans;
-    }
-}
